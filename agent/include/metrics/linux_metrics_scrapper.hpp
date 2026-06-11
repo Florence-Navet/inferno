89ec4656e2cf361f2857e0f9fbeba9dbbcf4e3f4
AdelinePat <adeline.patenne@hotmail.fr>
2026-06-11 22:49:04 +0200

    feat: readMem and readCpu methods for LinuxMetricsScrapper

--- agent/include/metrics/linux_metrics_scrapper.hpp
+++ agent/include/metrics/linux_metrics_scrapper.hpp
@@ -1,8 +1,9 @@
 #ifndef LINUX_METRICS_SCRAPPER_HPP
 #define LINUX_METRICS_SCRAPPER_HPP
 
-#include "metrics/i_metrics_scrapper.hpp"
 #include <map>
+
+#include "metrics/i_metrics_scrapper.hpp"
 
 class LinuxMetricsScrapper : public IMetricsScrapper {
  public:
@@ -21,6 +22,9 @@ class LinuxMetricsScrapper : public IMetricsScrapper {
   MemSample readMem();
   std::vector<DiskSample> readDisks();
   std::vector<NetSample> readNet();
+  std::ifstream openFile(const std::string& path);
+  float computeCpuPercent(const RawCpuSnapshot& snapshot, const std::size_t& index);
+  RawCpuSnapshot getRawCpuSnapshot();
 };
 
 #endif

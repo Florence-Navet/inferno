2b44d6631bb651cf3cc3f7c1793ac7b9cc69cbbc
Florence Navet <enolaworld@gmail.com>
2026-07-17 15:30:06 +0200

    feat(dashboard): add agent list and output view with hardcoded data

--- dashboard/mainwindow.cpp
+++ dashboard/mainwindow.cpp
@@ -6,9 +6,21 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    ui->setupUi(this);
+    populateAgents();
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::populateAgents()
+{
+    // TODO: replace with RegisterPayload list from DataType::AGENTS
+    ui->agentList->addItem("agent1-desktop");
+    ui->agentList->addItem("agent2-srv");
+    ui->agentList->addItem("agent3-lab");
+    ui->agentList->addItem("agent4-pi");
+    ui->agentList->addItem("agent5-mac");
 }

84f2dc72d17e686f73fdd977493bc40b08baa5eb
Florence Navet <enolaworld@gmail.com>
2026-07-18 15:16:38 +0200

    feat(dashboard): add metrics cards row and scrollable content area

--- dashboard/mainwindow.h
+++ dashboard/mainwindow.h
@@ -4,6 +4,16 @@
 #include <QMainWindow>
 #include <QString>
 
+#include <QMainWindow>
+#include <QString>
+
+#include <QHash>
+
+class QLabel;
+
+QT_BEGIN_NAMESPACE
+
+
 QT_BEGIN_NAMESPACE
 namespace Ui {
 class MainWindow;
@@ -13,6 +23,7 @@ QT_END_NAMESPACE
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
+
 
 public:
     MainWindow(QWidget *parent = nullptr);
@@ -29,6 +40,20 @@ private:
     /// Displays command output. TODO: feed with DashboardResponse data.
     void showOutput(const QString &text);
 
+    ///Builds the metric cards, table and console inside contentArea
+    void buildContentArea();
+
+    /// Creates one metric card (title + big value + subtitle).
+    QWidget *createMetricCard(const QString &key, const QString &title, const QString &value, const QString &subtitle);
+
+    /// Creates a QLabel with the given text and object name.QWidget *createMetricCard(const QString &key, const QString &title, const QString &value, const QString &subtitle);
+    QLabel *makeLabel(const QString &text, const QString &objectName);
+
     QString m_target;
+
+    QHash<QString, QLabel *> m_metricValues;
+
+    /// Updates a metric card value by key. TODO: call from server DataPayload.
+    void updateMetric(const QString &key, const QString &value);
 };
 #endif // MAINWINDOW_H

8580dce51aa4f8ccc943f71f0089c7bcae301e96
Florence Navet <enolaworld@gmail.com>
2026-07-19 14:20:53 +0200

    feat(dashboard): support multiple series whith legend in LineChartWidget

--- dashboard/linechartwidget.h
+++ dashboard/linechartwidget.h
@@ -12,14 +12,18 @@ class LineChartWidget : public QWidget
 public:
     explicit LineChartWidget(QWidget *parent = nullptr);
     void setData(const QVector<double> &data);
-     void setTitle(const QString &title);
+    void setTitle(const QString &title);
+    void setSeries(const QVector<QVector<double>> &series);
+    void setLabels(const QStringList &labels);
 
 protected:
     void paintEvent(QPaintEvent *event) override;
 
 private:
-    QVector<double> m_data;
+    QVector<double> m_data; // a list of number -> one one ligne
+    QVector<QVector<double>> m_series;   // one inner vector per curve
     QString m_title;
+    QStringList m_labels;   // one name per series, for the legend
 
 };
 

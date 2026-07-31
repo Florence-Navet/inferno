a92353046d81421356ef7557d37a0acc089200a5
Florence <enolaworld@gmail.com>
2026-07-31 15:32:09 +0200

    feat(dashboard): fill metric card subtitles from live samples

--- dashboard/src/metriccardswidget.cpp
+++ dashboard/src/metriccardswidget.cpp
@@ -32,12 +32,17 @@ QWidget *MetricCardsWidget::createMetricCard(const QString &key, const QString &
 {
     QFrame *card = new QFrame;
     card->setObjectName("metricCard");
+
     QLabel *valueLabel = makeLabel(value, "metricValue");
     m_metricValues.insert(key, valueLabel);
+
     QVBoxLayout *layout = new QVBoxLayout(card);
     layout->addWidget(makeLabel(title, "metricTitle"));
     layout->addWidget(valueLabel);
-    layout->addWidget(makeLabel(subtitle, "metricSubtitle"));
+
+    QLabel *subtitleLabel = makeLabel(value, "metricSubtitle");
+    m_metricSubtitles.insert(key, subtitleLabel);
+    layout->addWidget(subtitleLabel);
 
     return card;
 }
@@ -46,8 +51,18 @@ void MetricCardsWidget::updateFromSample(const MetricsSample &sample)
 {
     updateMetric("cpu", QString::number(sample.cpu.total_percent, 'f', 1) + "%");
 
+    updateSubtitle("cpu",
+                   QString::number(static_cast<int>(sample.cpu.per_core.size()))
+                       + " cores");
+
     const double gb = sample.mem.phys_used / 1024.0 / 1024.0 / 1024.0;
     updateMetric("memory", QString::number(gb, 'f', 1) + " GB");
+
+    const double totalGb = sample.mem.phys_total / 1024.0 / 1024.0 / 1024.0;
+    const double swapGb = sample.mem.swap_used / 1024.0 / 1024.0 / 1024.0;
+    updateSubtitle("memory", "of " + QString::number(totalGb, 'f', 1)
+                                 + " GB · swap "
+                                 + QString::number(swapGb, 'f', 1));
 
     double diskRead = 0.0;
     for (const DiskSample &disk : sample.disks) diskRead += disk.read_bytes_per_sec;
@@ -55,13 +70,33 @@ void MetricCardsWidget::updateFromSample(const MetricsSample &sample)
     diskRead = diskRead / 1024.0 / 1024.0;
     updateMetric("disk", QString::number(diskRead, 'f', 1) + " MB/s");
 
-    double netRx = 0.0;
-    for (const NetSample &iface : sample.interfaces) netRx += iface.rx_bytes_per_sec;
+    double diskWrite = 0.0;
+    for (const DiskSample &disk : sample.disks) diskWrite += disk.write_bytes_per_sec;
 
-    if (netRx > 1024.0 * 1024.0 * 1024.0) {
-        qDebug() << "network rx out of range, sample skipped:" << netRx;
-    } else {
-        netRx = netRx / 1024.0;
-        updateMetric("network", QString::number(netRx, 'f', 1) + " KB/s");
+    diskWrite = diskWrite / 1024.0 / 1024.0;
+    updateSubtitle("disk", "write " + QString::number(diskWrite, 'f', 1) + " MB/s");
+
+
+    double netRx = 0.0;
+    double netTx = 0.0;
+    for (const NetSample &iface : sample.interfaces) {
+        netRx += iface.rx_bytes_per_sec;
+        netTx += iface.tx_bytes_per_sec;
     }
+
+    const double maxRate = 1024.0 * 1024.0 * 1024.0;
+    if (netRx > maxRate || netTx > maxRate) {
+        qDebug() << "network rate out of range, sample skipped:" << netRx << netTx;
+    } else {
+        updateMetric("network", QString::number(netRx / 1024.0, 'f', 1) + " KB/s");
+        updateSubtitle("network", "tx " + QString::number(netTx / 1024.0, 'f', 1) + " KB/s");
+    }
+}
+
+void MetricCardsWidget::updateSubtitle(const QString &key, const QString &value)
+{
+    if (m_metricSubtitles.contains(key))
+        m_metricSubtitles[key]->setText(value);
+
+
 }
